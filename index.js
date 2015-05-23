e19a481222cd3ac94d3fa0e7dfc5db932139cf20
Mario Uher <uher.mario@gmail.com>
2015-05-23 12:20:51 +0200

    Implements bacon-is-awesome.

--- index.js
+++ index.js
@@ -1,25 +1,96 @@
 import Rx, { Observable, BehaviorSubject } from 'rx';
+import React, { Component, PropTypes } from 'react';
 
 
-
-let targetValue = (e) => e.target.value;
-let presence = (v) => !!v.length;
-let and = (a, b) => a && b;
-
-
-let usernamePresence = Observable.fromEvent(document.querySelector('input[name=username]'), 'keyup')
-  .map(targetValue)
-  .map(presence);
-
-let fullnamePresence = Observable.fromEvent(document.querySelector('input[name=fullname]'), 'keyup')
-  .map(targetValue)
-  .map(presence);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: new BehaviorSubject('') };
+  }
 
 
-let button = document.querySelector('button');
-let bothPresence = Observable.combineLatest(usernamePresence, fullnamePresence, and);
+  render() {
+    return (
+      <div>
+        <TextInput text={this.state.text} />
+        <Label text={this.state.text} />
+        <InvertedLabel text={this.state.text} />
+      </div>
+    );
+  }
+}
 
 
-let buttonEnabled = new BehaviorSubject(false);
-bothPresence.subscribe(buttonEnabled);
-buttonEnabled.subscribe((enabled) => button.disabled = !enabled);
+class TextInput extends Component {
+  static propTypes = {
+    text: PropTypes.instanceOf(BehaviorSubject).isRequired
+  }
+
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+
+  render() {
+    return <input
+              type="text"
+              ref="input"
+              value={this.state.text}
+              onChange={()=>this.handleChange()}
+            />;
+  }
+
+
+  handleChange() {
+    let text = React.findDOMNode(this.refs.input).value;
+
+    this.setState({ text });
+    this.props.text.onNext(text);
+  }
+}
+
+
+class Label extends React.Component {
+  static propTypes = {
+    text: PropTypes.instanceOf(BehaviorSubject).isRequired
+  }
+
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+
+  componentWillMount() {
+    this.props.text.subscribe((text) => {
+      this.setState({ text });
+    });
+  }
+
+
+  render() {
+    return <p>{this.state.text}</p>;
+  }
+}
+
+
+class InvertedLabel extends Label {
+  static propTypes = {
+    text: PropTypes.instanceOf(BehaviorSubject).isRequired
+  }
+
+
+  componentWillMount() {
+    let reverse = (string) => string.split('').reverse().join('');
+
+    this.props.text.map(reverse).subscribe((text) => {
+      this.setState({ text });
+    });
+  }
+}
+
+
+React.render(<App />, document.body);
